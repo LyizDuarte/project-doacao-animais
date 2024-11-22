48908dba03719a76fd5d88b5a6ca3a9f73dc2f0e
Luiz Duarte <looduarte21@gmail.com>
2024-11-22 13:59:15 -0300

    feat: rota para listagem de animais adotados

--- routes/adocaoRoute.js
+++ routes/adocaoRoute.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const AdocaoController = require("../controllers/adocaoController")
+const AuthMiddleware = require("../middlewares/authMiddleware")
 
 const router = express.Router()
+let auth = new AuthMiddleware()
 
 let controller = new AdocaoController()
 router.get("/", controller.listarView)
+router.get("/listar", auth.verificarUsuarioLogado, controller.adotadosView)
 router.post("/adotar", controller.adotar)
 
 module.exports = router
