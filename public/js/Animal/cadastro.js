c6bc52544d1ddff50a3d66cd3cce7352f2ff9d0b
LyizDuarte <looduarte21@gmail.com>
2024-11-26 19:36:02 -0300

    feat: reload de tela ao cadastrar animal

--- public/js/Animal/cadastro.js
+++ public/js/Animal/cadastro.js
@@ -1,36 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var btnGravar = document.getElementById("btnGravar")
+  var btnGravar = document.getElementById("btnGravar");
 
-  var inputImg = document.getElementById("inputImagem")
+  var inputImg = document.getElementById("inputImagem");
 
-  inputImg.addEventListener("change", exibirPrevia)
-  btnGravar.addEventListener("click", gravarAnimal)
-})
+  inputImg.addEventListener("change", exibirPrevia);
+  btnGravar.addEventListener("click", gravarAnimal);
+});
 
 function exibirPrevia(e) {
   //carrega a imagem na div previaImagem!
   //ler a imagem;
-  let imagem = document.getElementById("inputImagem").files[0]
+  let imagem = document.getElementById("inputImagem").files[0];
 
   //validando extensão
-  let ext = imagem.type.split("/").pop()
+  let ext = imagem.type.split("/").pop();
   if (ext == "png" || ext == "jpg" || ext == "jpeg") {
-    let imgPrevia = document.getElementById("imgPrevia")
-    let objImg = URL.createObjectURL(imagem)
-    imgPrevia.setAttribute("src", objImg)
-    document.getElementById("previaImagem").style.display = "block"
+    let imgPrevia = document.getElementById("imgPrevia");
+    let objImg = URL.createObjectURL(imagem);
+    imgPrevia.setAttribute("src", objImg);
+    document.getElementById("previaImagem").style.display = "block";
   } else {
-    alert("A extensão da imagem é inválida!")
-    document.getElementById("inputImagem").value = ""
+    alert("A extensão da imagem é inválida!");
+    document.getElementById("inputImagem").value = "";
   }
 }
 
 function gravarAnimal() {
-  var inputNome = document.getElementById("inputNome")
-  var inputDescricao = document.getElementById("inputDescricao")
-  var inputFile = document.getElementById("inputImagem").files[0]
-  var inputDisponivel = document.getElementById("selDisponivel")
-  var selTipo = document.getElementById("selTipo")
+  var inputNome = document.getElementById("inputNome");
+  var inputDescricao = document.getElementById("inputDescricao");
+  var inputFile = document.getElementById("inputImagem").files[0];
+  var inputDisponivel = document.getElementById("selDisponivel");
+  var selTipo = document.getElementById("selTipo");
 
   //if de validação básica
   if (
@@ -49,32 +49,33 @@ function gravarAnimal() {
             categoria: selCategoria.value
         }*/
 
-    var data = new FormData()
-    data.append("nome", inputNome.value)
-    data.append("descricao", inputDescricao.value)
-    data.append("tipo", selTipo.value)
-    data.append("disponivel", selDisponivel.checked ? 1 : 0)
-    data.append("imagem", inputFile)
+    var data = new FormData();
+    data.append("nome", inputNome.value);
+    data.append("descricao", inputDescricao.value);
+    data.append("tipo", selTipo.value);
+    data.append("disponivel", selDisponivel.checked ? 1 : 0);
+    data.append("imagem", inputFile);
 
     fetch("/animal/cadastrar", {
       method: "POST",
       body: data,
     })
       .then((r) => {
-        return r.json()
+        return r.json();
       })
       .then((r) => {
         if (r.ok) {
-          alert("Animal cadastrado!")
+          alert("Animal cadastrado!");
+          window.location.href = "/animal";
         } else {
-          alert("Erro ao cadastrar animal")
+          alert("Erro ao cadastrar animal");
         }
       })
       .catch((e) => {
-        console.log(e)
-      })
+        console.log(e);
+      });
   } else {
-    alert("Preencha todos os campos corretamente!")
-    return
+    alert("Preencha todos os campos corretamente!");
+    return;
   }
 }
