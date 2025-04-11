6ac60fe5794a7a88803844081383f5fa70d81283
LyizDuarte <looduarte21@gmail.com>
2025-04-11 10:58:50 -0300

    feat: ajustes para deploy

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,28 +1,21 @@
-const UsuarioModel = require("../models/usuarioModel");
-
-
+const UsuarioModel = require("../models/usuarioModel")
 class AuthMiddleware {
-
-    async verificarUsuarioLogado(req, res, next) {
-        if(req.cookies != undefined && req.cookies.usuarioLogado != null){
-            let usuarioId = req.cookies.usuarioLogado;
-            let usuario = new UsuarioModel();
-            usuario = await usuario.obter(usuarioId);
-            if(usuario != null && usuario.usuarioAtivo == 1) {
-                //o locals na resposta vai injetar a variavel usuarioLogado em todas as páginas que dependem do middleware para liberar o acesso
-                res.locals.usuarioLogado = usuario;
-                next();
-                
-            }
-            else{
-                res.redirect("/login");
-            }
-        }
-        else{
-            res.redirect("/login");
-        }
+  async verificarUsuarioLogado(req, res, next) {
+    if (req.cookies != undefined && req.cookies.usuarioLogado != null) {
+      let usuarioId = req.cookies.usuarioLogado
+      let usuario = new UsuarioModel()
+      usuario = await usuario.obter(usuarioId)
+      if (usuario != null && usuario.usuarioAtivo == 1) {
+        //o locals na resposta vai injetar a variavel usuarioLogado em todas as páginas que dependem do middleware para liberar o acesso
+        res.locals.usuarioLogado = usuario
+        next()
+      } else {
+        res.redirect("/login")
+      }
+    } else {
+      res.redirect("/login")
     }
-
+  }
 }
 
-module.exports = AuthMiddleware;
+module.exports = AuthMiddleware
